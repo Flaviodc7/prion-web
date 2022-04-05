28490ac7dc21579238f14725625227aebbe6d5c1
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-05 09:34:20 -0300

    Añadiendo iconos y contenido al Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { socials } from "./footer-list";
 
 const Footer = () => {
   return (
     <footer>
-        
+      <div className="footer__socials">
+        {socials.map(({ id, link, icon, alt }) => {
+          return (
+            <a key={id} href={link} target="_blank" rel="noopener noreferrer" alt={alt}>
+              {icon}
+            </a>
+          );
+        })}
+      </div>
+      <div className="footer__copyright">
+        <small>
+          &copy; Prion 2022. Todos los derechos reservados
+        </small>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/footer/footer-list.js
+++ src/components/footer/footer-list.js
@@ -1,26 +1,30 @@
+import { BsYoutube, BsFacebook } from "react-icons/bs"
+import { FaBandcamp } from "react-icons/fa"
+import { IoMdMail } from "react-icons/io"
+
 export const socials = [
     {
         id: 1,
         link: "https://www.facebook.com/Priondeathmetal",
-        icon: "",
+        icon: <BsFacebook />,
         alt: "Facebook Official",
     },
     {
         id: 2,
         link: "https://prion.bandcamp.com/",
-        icon: "Bandcamp Official",
+        icon: <FaBandcamp />,
         alt: "Bandcamp Official",
     },
     {
         id: 3,
         link: "http://www.youtube.com/user/priondeath",
-        icon: "",
+        icon: <BsYoutube />,
         alt: "YouTube Official"
     },
     {
         id: 3,
         link: "mailto:priondeth@yahoo.com",
-        icon: "",
+        icon: <IoMdMail />,
         alt: "Mail"
     },
 ]
